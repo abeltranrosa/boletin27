d5be3bdb5c97dc0dce47906d7ddb1ab51b8e4c57
antonio <antonio@192.168.0.11>
2017-06-05 13:02:23 +0200

    boletin27 interfaz

--- src/boletin27/Interfaz.java
+++ src/boletin27/Interfaz.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package boletin27;
 
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author Julian
- */
+
 public class Interfaz extends javax.swing.JFrame {
 
     
